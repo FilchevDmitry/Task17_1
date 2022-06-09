e4b29c0c1fdc517c8b0fbe48fa6d91ba2b03601e
DmityFilchev <filchev_dmitry@mail.ru>
2022-06-09 16:13:34 +0300

    Добавьте файлы проекта.

---
+++ Task17_1/Task17_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+void sw(int *a, int *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+int main()
+{
+	int a;
+	int b;
+	std::cout << "Enter 2 numbers :";
+	std::cin >> a >> b;
+	sw(&a, &b);
+	std::cout << a << " " << b<<std::endl;
+}
+
